285e0f81fffd934918695a618b6f070e212dfbbe
Simon KP <si@eskp.net>
2015-02-24 13:54:59 +1100

    Move to puppetmaster daemon

--- bootstrap/ubuntu-puppetmaster.sh
+++ bootstrap/ubuntu-puppetmaster.sh
@@ -20,12 +20,10 @@ wget --output-document="${repo_deb_path}" "${REPO_DEB_URL}" 2>/dev/null
 dpkg -i "${repo_deb_path}" >/dev/null
 apt-get update >/dev/null
 
-echo "Installing Puppet Master under Passenger..."
-apt-get install -y puppetmaster-passenger >/dev/null
-# Autosign certificates
-echo '*.boxnet' > /etc/puppet/autosign.conf
+echo "Installing Puppet Master..."
+apt-get install -y puppetmaster >/dev/null
 puppet agent --enable
-echo "Puppet installed!"
+echo "Puppet installed and running."
 
 # Generate Deploy key to be able to clone repos from Gitlab
 ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa
--- puppet/vagrant.pp
+++ puppet/vagrant.pp
@@ -3,8 +3,29 @@
 node default {
   class { '::r10k': }
   class { '::hiera': }
-  create_resources(service, hiera('enable_puppet'))
-  create_resources(ini_setting, hiera('settings'))
+  create_resources(ini_setting, hiera('ini_settings'))
   create_resources(sshkey, hiera('ssh_keys'))
-  create_resources(git_deploy_key, hiera('deploy_keys'))
+  create_resources(git_deploy_key, hiera('git_deploy_keys'))
+
+  # Basic Autosigning of certificates
+
+  file { '/etc/puppet/autosign.conf':
+    content => '*.boxnet',
+  }
+
+  # Hack below to start puppetmaster daemon. Receiving an error:
+  #
+  # puppet-master: (/File[/etc/puppet/environments/production]/ensure)
+  # change from absent to directory failed: Could not set 'directory' on ensure:
+  # Permission denied - /etc/puppet/environments/production
+
+  file { 'environment':
+    path   => '/etc/puppet/environments/production',
+    ensure => directory,
+  }
+  service { 'puppetmaster':
+    enable  => true,
+    ensure  => running,
+    require => File['environment'],
+  }
 }
